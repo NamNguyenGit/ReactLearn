f3cc97064240c0a83b54854b091d2f2947c4eaaa
NamNguyenGit <namnh.bi10-123@st.usth.edu.vn>
2021-11-22 17:16:56 +0700

    Update Service

--- ShoppingCart/server/routes/post.js
+++ ShoppingCart/server/routes/post.js
@@ -3,7 +3,23 @@ const router = express.Router();
 const Post = require("../models/Post");
 const verifyToken = require("../middleware/auth");
 
-// @route POST api/auth/posts
+// @route GET api/posts
+// @desc GET posts
+// @@access Private
+router.get("/", verifyToken, async (req, res) => {
+  try {
+    const posts = await Post.find({ user: req.userId }).populate("user", [
+      "username",
+      "createdAt",
+    ]);
+    res.json({ success: true, posts });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+// @route POST api/posts
 // @desc Create posts
 // @@access Private
 
@@ -27,6 +43,49 @@ router.post("/", verifyToken, async (req, res) => {
       success: true,
       message: "Woah! New trend buddy!",
       post: newPost,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ success: false, message: "Internal server error" });
+  }
+});
+
+// @route PUT api/posts
+// @desc Update posts
+// @@access Private
+
+router.put("/:id", verifyToken, async (req, res) => {
+  const { ServiceName, ServicePrice } = req.body;
+
+  //Simple validate
+  if (!ServiceName)
+    return res
+      .status(400)
+      .json({ success: false, message: "Missing Service Name" });
+
+  try {
+    let updatedPost = {
+      ServiceName,
+      ServicePrice,
+    };
+
+    const postUpdateCondition = { _id: req.params.id, user: req.userId };
+    updatedPost = await Post.findOneAndUpdate(
+      postUpdateCondition,
+      updatedPost,
+      { new: true }
+    );
+    //User not authorise to update post or post not found
+    if (!updatedPost)
+      return res.status(401).json({
+        success: false,
+        message: "Post not found or user not authorised",
+      });
+
+    res.json({
+      success: true,
+      message: "Updated successfully",
+      post: updatedPost,
     });
   } catch (error) {
     console.log(error);
