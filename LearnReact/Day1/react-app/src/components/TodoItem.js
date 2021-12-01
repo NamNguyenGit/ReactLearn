a0f7f1603be21715adc0f2e7becc2a19c4ce14f7
NamNguyenGit <namnh.bi10-123@st.usth.edu.vn>
2021-12-01 23:38:14 +0700

    FE

--- LearnReact/Day1/react-app/src/components/TodoItem.js
+++ LearnReact/Day1/react-app/src/components/TodoItem.js
@@ -2,7 +2,19 @@ import React from "react";
 
 const TodoItem = (props) => {
   const todo = props.todoProps;
-  return <p> {todo}</p>;
+  return (
+    <p>
+      <div class="checkbox">
+        <label>
+          <input type="checkbox" value="">
+            {todo}
+          </input>
+        </label>
+        <button>Delete</button>
+      </div>
+      {todo}
+    </p>
+  );
 };
 
 export default TodoItem;
