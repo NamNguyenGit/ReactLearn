f0770fe1e742558b86c125bdf5bf2e845c654460
NamNguyenGit <namnh.bi10-123@st.usth.edu.vn>
2021-12-03 20:17:04 +0700

    setTodoState

--- LearnReact/Day1/react-app/src/components/TodoItem.js
+++ LearnReact/Day1/react-app/src/components/TodoItem.js
@@ -1,7 +1,9 @@
 import React from "react";
 import { Button } from "reactstrap";
+import PropTypes from "prop-types";
 const TodoItem = (props) => {
   const todo = props.todoProps;
+  const markComplete = props.markDonefunction;
   //Style
   const todoItemStyle = {
     background: "#f4f4f4",
@@ -22,11 +24,21 @@ const TodoItem = (props) => {
   //return
   return (
     <p style={todoItemStyle}>
-      <input type="checkbox" />
+      <input
+        type="checkbox"
+        onChange={markComplete.bind(this, todo.id)}
+        checked={todo.status}
+      />
       {todo.title}
       <Button style={deletebtn}>Delete</Button>
     </p>
   );
+};
+
+//PropTypes
+TodoItem.propTypes = {
+  todoProps: PropTypes.object.isRequired,
+  markDonefunction: PropTypes.func.isRequired,
 };
 
 export default TodoItem;
--- LearnReact/Day1/react-app/src/components/Todos.js
+++ LearnReact/Day1/react-app/src/components/Todos.js
@@ -4,14 +4,17 @@ import TodoItem from "./TodoItem";
 const Todos = () => {
   const [todoStates, settodoStates] = useState([
     {
+      id: 1,
       title: "Eat breakfast",
       status: false,
     },
     {
+      id: 2,
       title: "Do homework",
       status: false,
     },
     {
+      id: 3,
       title: "Go to sleep",
       status: false,
     },
@@ -21,10 +24,20 @@ const Todos = () => {
   //   for (let todo of todoStates) {
   //     allTodos.push(<p>{todo}</p>);
   //   }
+
+  const markDone = (id) => {
+    const newTodo = todoStates.map((todo) => {
+      if (todo.id === id) todo.status = !todo.status;
+      return todo;
+    });
+
+    settodoStates(newTodo);
+  };
+
   return (
     <Fragment>
       {todoStates.map((todo) => {
-        return <TodoItem todoProps={todo} />;
+        return <TodoItem todoProps={todo} markDonefunction={markDone} />;
       })}
     </Fragment>
   );
